45440779ec3ccf96b661a633ffd36839c31a8bfd
James Brundage <@github.com>
2022-12-24 17:01:21 -0800

    Adding 4bitcss.build.ps1 (Fixes #3)

---
+++ 4bitcss.build.ps1
@@ -0,0 +1,20 @@
+if ($env:GITHUB_WORKSPACE) {
+    git clone https://github.com/mbadolato/iTerm2-Color-Schemes.git | Out-Host
+
+    $jsonFiles = Get-ChildItem -Path iTerm2-Color-Schemes -Recurse -Filter *.json  |
+        Where-Object Fullname -like '*terminal*' | 
+        Select-Object -Skip 2    
+} else {
+    $jsonFiles = Get-ChildItem $home\documents\git\iTerm2-Color-Schemes -Recurse -Filter *.json  |
+        Where-Object Fullname -like '*terminal*' | 
+        Select-Object -Skip 2
+}
+
+
+Import-Module .\4bitcss.psd1 -Global
+
+foreach ($jsonFile in $jsonFiles) {
+    $jsonObject = [IO.File]::ReadAllText($jsonFile.FullName) | ConvertFrom-Json
+    $jsonObject | Export-4BitCSS
+    $jsonObject | Export-4BitCSS -OutputPath (Join-Path $pwd docs)    
+}
