00686f78252dd4fc97ea29d13db568a53ab5c9f0
James Brundage <@github.com>
2022-12-27 16:44:53 -0800

    Adding preview pages (Fixes #10)

--- 4bitcss.build.ps1
+++ 4bitcss.build.ps1
@@ -13,11 +13,25 @@ if ($env:GITHUB_WORKSPACE) {
 
 Import-Module .\4bitcss.psd1 -Global
 
+$transpiledPreview = Build-PipeScript -InputPath .\docs\index.ps.markdown
+$transpiledText = [IO.File]::ReadAllText($transpiledPreview.FullName)
+
+$docsPath = Join-Path $PSScriptRoot docs
 foreach ($jsonFile in $jsonFiles) {
     $jsonObject = [IO.File]::ReadAllText($jsonFile.FullName) | ConvertFrom-Json
 
     if (-not $jsonObject.Name) { continue }
     if ($jsonObject.Name -match '^\{') { continue }
     $jsonObject | Export-4BitCSS -OutputPath (Join-Path $PSScriptRoot css)
-    $jsonObject | Export-4BitCSS -OutputPath (Join-Path $PSScriptRoot docs)
+    $jsonObject | Export-4BitCSS -OutputPath $docsPath
+    $previewFilePath = Join-Path $docsPath "$($jsonObject.Name).md"
+@"
+---
+title: $($jsonObject.Name)
+stylesheet: $($jsonObject.Name).css
+---
+$transpiledText
+"@ |
+    Set-Content $previewFilePath -Encoding utf8
+    Get-Item -Path $previewFilePath
 }
