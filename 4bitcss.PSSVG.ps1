f11df4c031424473b63c4675c1dec5f23c4e0254
James Brundage <@github.com>
2022-12-28 17:34:58 -0800

    Vertical fencepost fix

--- 4bitcss.PSSVG.ps1
+++ 4bitcss.PSSVG.ps1
@@ -57,7 +57,7 @@ $boxSize = [Ordered]@{Width = 80; Height = 80}
         =<svg.rect> -X ($boxSize.Width * $n) -Y 0 -Class "ansi$n-fill" @boxSize
     }
     foreach ($n in 8..15) {
-        =<svg.rect> -X ($boxSize.Width * ($n - 8)) -Y 80 -Class "ansi$n-fill" @boxSize
+        =<svg.rect> -X ($boxSize.Width * ($n - 8)) -Y 79 -Class "ansi$n-fill" @boxSize
     }
 ) -OutputPath (Join-Path $docsRoot .\4bitpreview.svg) -Width 320
 
