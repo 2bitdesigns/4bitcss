8954d25748a7a7669929544e0aaa2cafce7f7896
James Brundage <@github.com>
2022-12-28 17:30:49 -0800

    Trying to remove slight line from preview template

--- 4bitcss.PSSVG.ps1
+++ 4bitcss.PSSVG.ps1
@@ -78,10 +78,10 @@ $colors = 'Black', 'Red', 'Green', 'Yellow', 'Blue', 'Purple', 'Cyan', 'White',
         '$ColorSchemeName'
     ) @fontSettings 
     foreach ($n in 0..7) {
-        =<svg.rect> -X ($boxSize.Width * $n) -Y 80 -Class "ansi$n-fill" @boxSize -Fill $($colors[$n])
+        =<svg.rect> -X ($boxSize.Width * $n) -Y 79 -Class "ansi$n-fill" @boxSize -Fill $($colors[$n])
     }
     foreach ($n in 8..15) {
-        =<svg.rect> -X ($boxSize.Width * ($n - 8)) -Y 160 -Class "ansi$n-fill" @boxSize -Fill $($colors[$n])
+        =<svg.rect> -X ($boxSize.Width * ($n - 8)) -Y 159 -Class "ansi$n-fill" @boxSize -Fill $($colors[$n])
     }
 ) -OutputPath (Join-Path $docsRoot .\4bitpreviewtemplate.svg) -Class background-fill
 
