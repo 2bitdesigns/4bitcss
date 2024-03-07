737b9ad0274b4618b537a7ed905b7bfd5f660956
James Brundage <@github.com>
2024-03-06 17:39:27 -0800

    fix: Replicating preview templates

--- 4bitcss.PSSVG.ps1
+++ 4bitcss.PSSVG.ps1
@@ -12,6 +12,11 @@ if (-not (Test-Path $docsRoot)) {
     $null = New-Item -ItemType Directory -Path $docsRoot
 }
 
+$includesRoot = Join-Path $docsRoot "_includes"
+if (-not (Test-Path $includesRoot)) {
+    $null = New-Item -ItemType Directory -Path $includesRoot
+}
+
 $fontSettings = [Ordered]@{
     TextAnchor        = 'middle'
     AlignmentBaseline = 'middle'
@@ -52,37 +57,43 @@ $assetFile | Copy-Item -Destination (Join-Path $docsRoot .\4bitcss.svg) -PassThr
 ) -OutputPath (Join-Path $docsRoot .\4bitpreview.svg) -Width 320 
 
 $boxSize = [Ordered]@{Width = 80; Height = 80}
-=<svg> -ViewBox 640, 160 @(
+svg -ViewBox 640, 160 @(
     foreach ($n in 0..7) {
-        =<svg.rect> -X ($boxSize.Width * $n) -Y 0 -Class "ansi$n-fill" @boxSize
+        svg.rect -X ($boxSize.Width * $n) -Y 0 -Class "ansi$n-fill" @boxSize
     }
     foreach ($n in 8..15) {
-        =<svg.rect> -X ($boxSize.Width * ($n - 8)) -Y 79 -Class "ansi$n-fill" @boxSize
+        svg.rect -X ($boxSize.Width * ($n - 8)) -Y 79 -Class "ansi$n-fill" @boxSize
     }
 ) -OutputPath (Join-Path $docsRoot .\4bitpreview.svg) -Width 320
+
+Copy-Item -Path (Join-Path $docsRoot .\4bitpreview.svg) -Destination (Join-Path $assetsRoot .\4bitpreview.svg) -Force -PassThru
+Copy-Item -Path (Join-Path $docsRoot .\4bitpreview.svg) -Destination (Join-Path $includesRoot .\4bitpreview.svg) -Force -PassThru
 
 $colors = 'Black', 'Red', 'Green', 'Yellow', 'Blue', 'Purple', 'Cyan', 'White',
     'BrightBlack', 'BrightRed', 'BrightGreen', 'BrightYellow', 'BrightBlue', 'BrightPurple', 'BrightCyan', 'BrightWhite'
 
-=<svg> -ViewBox 640, 240 @(
-    =<svg.defs> @(
-        =<svg.style> -Type 'text/css' @'
+svg -ViewBox 640, 240 @(
+    svg.defs @(
+        svg.style -Type 'text/css' @'
 @import url('/$ColorSchemeFileName/$ColorSchemeFileName.css')
 '@
-        =<svg.style> -Type 'text/css' @'
+        svg.style -Type 'text/css' @'
 @import url('https://fonts.googleapis.com/css?family=Abel')
 '@    
     )
-    =<svg.rect> -Width 640 -Height 80 -X 0 -Y 0 -Class 'background-fill'
-    =<svg.text> -X 50% -Y 16.5% -Class 'foreground-fill' -Content @(
+    svg.rect -Width 640 -Height 80 -X 0 -Y 0 -Class 'background-fill'
+    svg.text -X 50% -Y 16.5% -Class 'foreground-fill' -Content @(
         '$ColorSchemeName'
     ) @fontSettings 
     foreach ($n in 0..7) {
-        =<svg.rect> -X ($boxSize.Width * $n) -Y 79 -Class "ansi$n-fill" @boxSize -Fill $($colors[$n])
+        svg.rect -X ($boxSize.Width * $n) -Y 79 -Class "ansi$n-fill" @boxSize -Fill $($colors[$n])
     }
     foreach ($n in 8..15) {
-        =<svg.rect> -X ($boxSize.Width * ($n - 8)) -Y 159 -Class "ansi$n-fill" @boxSize -Fill $($colors[$n])
+        svg.rect -X ($boxSize.Width * ($n - 8)) -Y 159 -Class "ansi$n-fill" @boxSize -Fill $($colors[$n])
     }
 ) -OutputPath (Join-Path $docsRoot .\4bitpreviewtemplate.svg) -Class background-fill
+
+Copy-Item -Path (Join-Path $docsRoot .\4bitpreviewtemplate.svg) -Destination (Join-Path $assetsRoot ..\4bitpreviewtemplate.svg) -Force -PassThru
+Copy-Item -Path (Join-Path $docsRoot .\4bitpreviewtemplate.svg) -Destination (Join-Path $includesRoot .\4bitpreviewtemplate.svg) -Force -PassThru
 
 Pop-Location
