99cc8c2a57e8230846aadc184233cbce35068acb
James Brundage <@github.com>
2024-03-16 11:15:27 -0700

    style: Adding Skip Back Icon

--- 4bitcss.build.ps1
+++ 4bitcss.build.ps1
@@ -142,6 +142,7 @@ Export-4BitSVG -SVG https://raw.githubusercontent.com/feathericons/feather/maste
 Export-4BitSVG -SVG https://raw.githubusercontent.com/feathericons/feather/master/icons/shuffle.svg -Stroke "ansi6" -OutputPath (Join-Path $IncludesPath "shuffle-icon.svg")
 Export-4BitSVG -SVG https://raw.githubusercontent.com/feathericons/feather/master/icons/help-circle.svg -Stroke "ansi6" -OutputPath (Join-Path $IncludesPath "help-circle-icon.svg")
 Export-4BitSVG -SVG https://raw.githubusercontent.com/feathericons/feather/master/icons/save.svg -OutputPath (Join-Path $IncludesPath "save-icon.svg")
+Export-4BitSVG -SVG https://raw.githubusercontent.com/feathericons/feather/master/icons/skip-back.svg -OutputPath (Join-Path $IncludesPath "skip-back-icon.svg")
 
 Get-Module 4bitcss | 
     Split-Path | 
