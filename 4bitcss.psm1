5330010b6c981afb99b9f8b4f6300576bc82ed56
StartAutomating <StartAutomating@github.com>
2024-04-20 21:23:44 +0000

    feat: Mounting 4bitcss ( Fixes #48 )

--- 4bitcss.psm1
+++ 4bitcss.psm1
@@ -13,4 +13,15 @@ $myModule = $MyInvocation.MyCommand.ScriptBlock.Module
 $ExecutionContext.SessionState.PSVariable.Set($myModule.Name, $myModule)
 $myModule.pstypenames.insert(0, $myModule.Name)
 
+$newDriveSplat = @{PSProvider='FileSystem';ErrorAction='Ignore';Scope='Global'}
+New-PSDrive -Name $MyModule.Name -Root ($MyModule | Split-Path) @newDriveSplat
+
+if ($home) {
+    $myMyModule = "My$($myModule.Name)"
+    $myMyModuleRoot = Join-Path $home $myMyModule
+    if (Test-Path $myMyModuleRoot) {
+        New-PSDrive -Name $myMyModule -Root $myMyModuleRoot @newDriveSplat
+    }
+}
+
 Export-ModuleMember -Function *-* -Alias * -Variable $myModule.Name
