e5e42af386d4266544f1558a8292a9109210528c
James Brundage <@github.com>
2024-04-20 14:15:44 -0700

    feat: Exposing $4bitcss ( Fixes #47 )

--- 4bitcss.ps.psm1
+++ 4bitcss.ps.psm1
@@ -1,1 +1,7 @@
 [Include('*-*.ps1')]$PSScriptRoot
+
+$myModule = $MyInvocation.MyCommand.ScriptBlock.Module
+$ExecutionContext.SessionState.PSVariable.Set($myModule.Name, $myModule)
+$myModule.pstypenames.insert(0, $myModule.Name)
+
+Export-ModuleMember -Function *-* -Alias * -Variable $myModule.Name
