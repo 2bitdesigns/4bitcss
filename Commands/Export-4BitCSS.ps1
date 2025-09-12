1c89adbb7d438d74b2a900308a7c551a0a9e93ac
StartAutomating <james.brundage@start-automating.com>
2025-09-12 13:20:20 -0700

    feat: Giving Credit ( Fixes #79 )
    Quoting CreditTo and CreditToLink

--- Commands/Export-4BitCSS.ps1
+++ Commands/Export-4BitCSS.ps1
@@ -336,7 +336,7 @@ function Export-4BitCSS
 :root {
   $(@(
     foreach ($prop in $jsonObject.psobject.properties) {
-        if ($prop.Name -eq 'Name') {
+        if ($prop.Name -eq 'Name', 'CreditTo','CreditToLink','FileName') {
             "--$($prop.Name): '$($prop.Value)'"
         } else {
             "--$($prop.Name): $($prop.Value)"
