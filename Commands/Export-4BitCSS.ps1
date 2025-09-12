99693b2118f35e34e82c57fda417027233b06377
StartAutomating <james.brundage@start-automating.com>
2025-09-12 13:46:41 -0700

    feat: Exporting PaletteName to CSS ( Fixes #122 )
    Quoting PaletteName and PaletteFileName

--- Commands/Export-4BitCSS.ps1
+++ Commands/Export-4BitCSS.ps1
@@ -336,7 +336,7 @@ function Export-4BitCSS
 :root {
   $(@(
     foreach ($prop in $jsonObject.psobject.properties) {
-        if ($prop.Name -in 'Name', 'CreditTo','CreditToLink','FileName') {
+        if ($prop.Name -in 'Name', 'CreditTo','CreditToLink','FileName','PaletteName','PaletteFileName') {
             "--$($prop.Name): '$($prop.Value)'"
         } else {
             "--$($prop.Name): $($prop.Value)"
